f86c62352772f306e8f77022b2b3b101f1ff9de6
sypark617 <62006521+sypark617@users.noreply.github.com>
2020-03-26 22:20:00 +0900

    todo
    done

---
+++ homework_WK3.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200309', headers=headers)
+
+print(data)
